d9589a59b58fe4906fbcd6ef0e60e213760cf8f8
Karol Fabjańczuk <karol.fabjanczuk@lemondemon.pl>
2015-05-06 15:02:17 +0200

    fix(rwd): refactor/fix responsive table

--- src/app/shared/table/table.js
+++ src/app/shared/table/table.js
@@ -2,39 +2,52 @@
 (function() {
     'use strict';
     angular.module('shared.table', [])
-        .directive('responsiveTable', responsiveTable);
+        .directive('responsiveTable', ['tableService', responsiveTable])
+        .service('tableService', tableService);
 
     ////////////////
     // Directives //
     ////////////////
-    function responsiveTable() {
+    function responsiveTable(tableService) {
         return {
             restrict: 'EA',
             scope: {},
             link: function postLink(scope, element) {
-                element.addClass('responsiveTable');
+                var $tableElement = element.closest('table');
 
-                element.each(function() {
-                    var headertext = [],
-                        headers = this.querySelectorAll('th'),
-                        tablebody = this.querySelector('tbody'),
-                        i, j, row, col, current;
+                $tableElement.addClass('responsiveTable');
+                tableService.addHeadersToRows($tableElement[0]);
+            }
+        };
+    }
 
-                    for (i = 0; i < headers.length; i++) {
-                        current = headers[i];
-                        headertext.push(current.textContent.replace(/\r?\n|\r/, ''));
-                    }
-                    if (tablebody && tablebody.rows && tablebody.rows.length) {
-                        for (i = 0; row = tablebody.rows[i]; i++) {
-                            for (j = 0; col = row.cells[j]; j++) {
-                                if (headertext[j]) {
-                                    col.setAttribute('data-th', headertext[j]);
-                                }
-                            }
+    //////////////
+    // Services //
+    //////////////
+    function tableService() {
+        function addHeadersToRows(element) {
+            var headertext = [],
+                headers = element.querySelectorAll('th'),
+                tablebody = element.querySelector('tbody'),
+                i, j, row, col, current;
+
+            for (i = 0; i < headers.length; i++) {
+                current = headers[i];
+                headertext.push(current.textContent.replace(/\r?\n|\r/, ''));
+            }
+            if (tablebody && tablebody.rows && tablebody.rows.length) {
+                for (i = 0; row = tablebody.rows[i]; i++) {
+                    for (j = 0; col = row.cells[j]; j++) {
+                        if (headertext[j]) {
+                            col.setAttribute('data-th', headertext[j]);
                         }
                     }
-                });
+                }
             }
+        }
+
+        return {
+            addHeadersToRows: addHeadersToRows
         };
     }
 })();
